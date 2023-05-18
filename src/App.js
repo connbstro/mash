932f61a1120a4cb434f6851ec4c46c692ee68e33
Connor S <constroh@gmail.com>
2023-05-18 17:24:40 -0500

    react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
